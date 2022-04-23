d711670399ce4a72c5b0bdc6f409804a7270f638
Jessica Brochu <jesbrochu@gmail.com>
2022-04-23 09:12:52 -0400

    feature(App): setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
-
   return (
     <div className="App">
-      My react app 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
