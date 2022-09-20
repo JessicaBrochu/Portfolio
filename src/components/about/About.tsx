93e3a1032e9c46ecc3cf9a960308b1f4a1cbc802
jessica.brochu <jesbrochu@gmail.com>
2022-09-20 15:53:15 -0400

    add about section

---
+++ src/components/about/About.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import './about.css'
+
+const About = () => {
+
+    return (
+      <div className="about">
+        <div className="wrapper">
+            <div className='about-content'>
+                <h3>À propos</h3>
+                <p>Bonjour,</p>
+                <p>je suis une <span>développeuse front-end</span> constamment à la recherche de nouveaux défis qui me permettront d’étoffer mes connaissances et ainsi progresser dans le domaine du web.</p>
+                <p>Ayant déjà réalisé un stage en tant que développeuse front-end, je suis à la recherche d’une expérience similaire dans <span>une agence qui recherche une développeuse motivée.</span></p>
+                <p>Concernant ma formation académique, je conclus présentement ma dernière année d’études. </p>
+                <p>On me décrit comme étant une personne autonome, motivée et organisée.</p>
+            </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default About
