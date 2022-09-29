4567b6ea32c841613e2a0a0e84cf2e3ed0450cf7
Jessica Brochu <jesbrochu@gmail.com>
2022-09-29 15:01:18 -0400

    add a form with EmailJS

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,15 +1,40 @@
-import * as React from 'react'
+import React from 'react';
+import emailjs from 'emailjs-com';
 import './contact.css'
 
 const Contact = () => {
 
-    return (
-      <div className="contact">
-        <div className="wrapper">
-            <h2>contact</h2>
-        </div>
-      </div>
-    )
+  function sendEmail(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_u62affi", "template_4gshdma",  e.currentTarget, "GBG2OZlOdP08Ivar3")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+      e.currentTarget.reset()
   }
-  
-  export default Contact
+
+  return (
+    <div className="contact">
+      <div className="wrapper">
+        <form className="contact-form" onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="name" />
+          <label>Email</label>
+          <input type="email" name="email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Contact
