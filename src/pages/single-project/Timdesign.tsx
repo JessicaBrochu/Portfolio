2160f78d2a18308c4b40d982fb6e5e3a8db7ae9d
Jessica Brochu <jesbrochu@gmail.com>
2022-10-10 09:56:10 -0400

    add: animations to each project

--- src/pages/single-project/Timdesign.tsx
+++ src/pages/single-project/Timdesign.tsx
@@ -2,38 +2,45 @@ import * as React from 'react'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
 import './single-project.css'
+// FIXME declare this module type
+//@ts-ignore
+import Fade from 'react-reveal/Fade';
 
 const Timdesign = () => {
 
-    return (
-      <div className="single-project single-timdesign">
-        <Header />
-        <div className="wrapper">
-            <h1>TIMDESIGN</h1>
-            <div className='single-project-infos'>
-              <div className='single-project-content'>
-                <nav>
-                  <ul>
-                    <li>HTML</li>
-                    <li>SCSS</li>
-                    <li>JavaScript</li>
-                    <li>Swiperjs</li>
-                  </ul>
-                </nav>
-                <p>Timdesign est un projet scolaire dont le but était de créer un site de conférence fictif visant à appliquer les connaissances apprises tout au long de la session.</p>
-                <p>Le but de ce projet était d’intégrer un site web avancé à l’aide d’HTML, CSS et JavaScript tout en s’occupant minutieusement du responsive. De plus, un slideshow devait être intégré à l’aide de le librairie Swiper.</p>
-                <div className='single-project-btn-ctn'>
-                  <a href="https://jbrochu.dectim.ca/timdesign/" target="_blank">Visiter le site</a>
-                </div>
-              </div>
-              <div className='single-project-media'>
-                <video loop muted autoPlay src="assets/timdesign.mp4"></video>
+  return (
+    <div className="single-project single-timdesign">
+      <Header />
+      <div className="wrapper">
+        <Fade left><h1>TIMDESIGN</h1></Fade>
+        <div className='single-project-infos'>
+          <Fade left>
+            <div className='single-project-content'>
+              <nav>
+                <ul>
+                  <li>HTML</li>
+                  <li>SCSS</li>
+                  <li>JavaScript</li>
+                  <li>Swiperjs</li>
+                </ul>
+              </nav>
+              <p>Timdesign est un projet scolaire dont le but était de créer un site de conférence fictif visant à appliquer les connaissances apprises tout au long de la session.</p>
+              <p>Le but de ce projet était d’intégrer un site web avancé à l’aide d’HTML, CSS et JavaScript tout en s’occupant minutieusement du responsive. De plus, un slideshow devait être intégré à l’aide de le librairie Swiper.</p>
+              <div className='single-project-btn-ctn'>
+                <a href="https://jbrochu.dectim.ca/timdesign/" target="_blank">Visiter le site</a>
               </div>
             </div>
+          </Fade>
+          <Fade right>
+            <div className='single-project-media'>
+              <video loop muted autoPlay src="assets/timdesign.mp4"></video>
+            </div>
+          </Fade>
         </div>
-        <Footer />
       </div>
-    )
-  }
-  
-  export default Timdesign
+      <Footer />
+    </div>
+  )
+}
+
+export default Timdesign
--- src/pages/single-project/Traveler.tsx
+++ src/pages/single-project/Traveler.tsx
@@ -2,40 +2,47 @@ import * as React from 'react'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
 import './single-project.css'
+// FIXME declare this module type
+//@ts-ignore
+import Fade from 'react-reveal/Fade';
 
 const Traveler = () => {
 
-    return (
-      
-      <div className="single-project single-traveler">
-        <Header />
-        <div className="wrapper">
-            <h1>TRAVELER</h1>
-            <div className='single-project-infos'>
-              <div className='single-project-content'>
-                <nav>
-                  <ul>
-                    <li>ReactJs</li>
-                    <li>TypeScript</li>
-                    <li>Material UI</li>
-                    <li>Vite</li>
-                  </ul>
-                </nav>
-                <p>Traveler est un projet personnel mettant en avant un site fictionnel proposant un service de réservation de résidence dans différents pays.</p>
-                <p>Le but de ce projet était de découvrir diverses technologies en un seul projet et principalement mettre en avant la composante de material UI Autocomplete. Mise à part material UI, j’ai expérimenté l’outil Vite pour démarrer mon projet. De plus, j’ai réalisé ce projet avec Reactjs ainsi que TypeScript.</p>
-                <div className='single-project-btn-ctn'>
-                  <a href="https://jbrochu-traveler.netlify.app/" target="_blank">Visiter le site</a>
-                  <a href="https://github.com/JessicaBrochu/Traveler" target="_blank">Visiter Github</a>
-                </div>
-              </div>
-              <div className='single-project-media'>
-                <video loop muted autoPlay src="assets/traveler.mp4"></video>
+  return (
+
+    <div className="single-project single-traveler">
+      <Header />
+      <div className="wrapper">
+        <Fade left><h1>TRAVELER</h1></Fade>
+        <div className='single-project-infos'>
+          <Fade left>
+            <div className='single-project-content'>
+              <nav>
+                <ul>
+                  <li>ReactJs</li>
+                  <li>TypeScript</li>
+                  <li>Material UI</li>
+                  <li>Vite</li>
+                </ul>
+              </nav>
+              <p>Traveler est un projet personnel mettant en avant un site fictionnel proposant un service de réservation de résidence dans différents pays.</p>
+              <p>Le but de ce projet était de découvrir diverses technologies en un seul projet et principalement mettre en avant la composante de material UI Autocomplete. Mise à part material UI, j’ai expérimenté l’outil Vite pour démarrer mon projet. De plus, j’ai réalisé ce projet avec Reactjs ainsi que TypeScript.</p>
+              <div className='single-project-btn-ctn'>
+                <a href="https://jbrochu-traveler.netlify.app/" target="_blank">Visiter le site</a>
+                <a href="https://github.com/JessicaBrochu/Traveler" target="_blank">Visiter Github</a>
               </div>
             </div>
+          </Fade>
+          <Fade right>
+            <div className='single-project-media'>
+              <video loop muted autoPlay src="assets/traveler.mp4"></video>
+            </div>
+          </Fade>
         </div>
-        <Footer />
       </div>
-    )
-  }
-  
-  export default Traveler
+      <Footer />
+    </div>
+  )
+}
+
+export default Traveler
