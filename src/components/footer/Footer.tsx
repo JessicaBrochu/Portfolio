dd82a6c428810ced8d183ef2dbf1d213d82f3d45
Jessica Brochu <jesbrochu@gmail.com>
2022-10-10 11:52:08 -0400

    add: animation to footer nav and fix arrow

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,8 +9,8 @@ const Footer = () => {
         <div className="wrapper">
             <nav className="footer-nav">
                 <ul>
-                  <li><Link to="/" state={{scrollId: '#hero'}}>ACCUEIL</Link></li>
-                    <li><Link to="/" state={{scrollId: '#projects'}}>PROJETS</Link></li>
+                  <li className='li-footer'><Link to="/" state={{scrollId: '#hero'}}>ACCUEIL</Link></li>
+                    <li className='li-footer'><Link to="/" state={{scrollId: '#projects'}}>PROJETS</Link></li>
                     <li className='li-logo'><Link to="/" state={{scrollId: '#hero'}} className="logo">
                       <svg width="30" height="30" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path d="M23.8629 0.990234V48.9787H19.3426L19.2 48.8773L0 35.4378V29.7477L19.2 43.19V6.72418L0 20.1664V14.4791L19.2 1.03685L19.2658 0.990234H23.8629Z" fill="white"/>
@@ -19,8 +19,8 @@ const Footer = () => {
                           <path d="M49.9205 29.748V29.8358L49.8574 29.7919L49.9205 29.748Z" fill="white"/>
                       </svg>
                     </Link></li>
-                    <li><Link to="/" state={{scrollId: '#about'}}>À PROPOS</Link></li>
-                    <li><Link to="/" state={{scrollId: '#contact'}}>CONTACT</Link></li>
+                    <li className='li-footer'><Link to="/" state={{scrollId: '#about'}}>À PROPOS</Link></li>
+                    <li className='li-footer'><Link to="/" state={{scrollId: '#contact'}}>CONTACT</Link></li>
                 </ul>
             </nav>
             <p>Jessica Brochu © Tous droits réservés</p>
